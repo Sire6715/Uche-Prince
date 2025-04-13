6d14153953e336946e9b7e59a0bb7876d175f169
Prince <uchepibe@gmail.com>
2025-04-14 00:37:43 +0100

    fix:fixed footer flex

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   return (
     <section
       id="Contact"
-      className="md:px-[6rem] md:my-[9rem] my-[3rem] md:p-[5rem] p-[2.5rem] h-56 md:flex flex-col-reverse  bg-[#f2f2f2] align-baseline justify-between items-center relative"
+      className="md:px-[6rem] md:my-[9rem] my-[3rem] md:p-[5rem] p-[2.5rem] h-56 md:flex md:flex-row flex-col-reverse  bg-[#f2f2f2] align-baseline justify-between items-center relative"
     >
       <div>
         <h1 className="md:text-7xl text-3xl flex flex-col font-black text-purple-500 w-1/2">
